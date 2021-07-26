88e275d6af12e433792c66f95f6684d8e3e71c39
dungbkhn <86149640+dungbkhn@users.noreply.github.com>
2021-07-26 23:42:20 +0700

    Add files via upload

--- getmd5hash_inremote.sh
+++ getmd5hash_inremote.sh
@@ -8,7 +8,7 @@ param=$(echo "$1" | tr -d '\n' | xxd -r -p)
 temp_rm=/home/backup/.temp
 
 if [ ! -f "$temp_rm"/md5 ] ; then
-	gcc -Wall -Wextra -O3 -D_LARGEFILE_SOURCE=1 -D_FILE_OFFSET_BITS=64 -o md5 md5.c
+	gcc -Wall -Wextra -O3 -D_LARGEFILE_SOURCE=1 -D_FILE_OFFSET_BITS=64 -o "$temp_rm"/md5 "$temp_rm"/md5.c
 fi
 
 if [ -f "$param" ] ; then
